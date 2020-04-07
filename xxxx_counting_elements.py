6cfe891ca6525e44e097b1234e8a2b631951cdf3
Lava-Swimmer <lxx4work@gmail.com>
2020-04-07 21:08:04 +0800

    Easy*1

---
+++ xxxx_counting_elements.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def countElements(self, arr: List[int]) -> int:
+        nums = set(arr)
+        count = 0
+        for item in arr:
+            if item + 1 in nums:
+                count += 1
+        return count
+
+
+if __name__ == '__main__':
+    solution = Solution()
+    assert solution.countElements([1, 2, 3]) == 2
+    assert solution.countElements([1, 1, 3, 3, 5, 5, 7, 7]) == 0
+    assert solution.countElements([1, 3, 2, 3, 5, 0]) == 3
+    assert solution.countElements([1, 1, 2, 2]) == 2
